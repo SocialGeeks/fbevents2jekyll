f853f017f3177b195d2f516c93b86070da32e9a3
Dan Daggett <socialgeek@gmail.com>
2014-03-18 22:16:04 -0600

    Quick hack to make script work with sg_events.json from FB graph.  This is going to need some more work.

--- convert.py
+++ convert.py
@@ -5,14 +5,14 @@ import urllib
 from datetime import datetime
 
 def getFile(name):
-	f = open(name, "r")
-	file = f.read()
-	f.close()
-	return file 
+    f = open(name, "r")
+    file = f.read()
+    f.close()
+    return file 
 
 def retrievePicture(source, destination):
-	if source is not None:	
-		urllib.urlretrieve(source, destination)
+    if source is not None:  
+        urllib.urlretrieve(source, destination)
 
 json_file = "sg_events.json"
 events = json.loads(getFile(json_file))
@@ -21,65 +21,66 @@ template_file = "template.md"
 template_original = getFile(template_file)
 
 for event in events["data"]:
-# name, description, start_time, end_time, location, host, venue, pic, pic_big, pic_cover, pic_small
+# name, description, start_time, end_time, location, host, venue, picture
 
-	print("[+] Processing event '" + event["name"] + "'")
+    print("[+] Processing event " + event["start_time"] + " - '" + event["name"] + "'")
 
-	template = template_original	
-	start_date = event["start_time"][:10]
+    template = template_original    
+    start_date = event["start_time"][:10]
 
-	file_name = start_date + "-" + event["name"].replace(" ", "-").replace("/", "").replace('"', "").replace("'", "")
-	pic_base = "output/public/images/thumbnails/"
-	pic = pic_base + file_name + ".jpg"
-	pic_big = pic_base + file_name + "_big.jpg"
-	pic_cover = pic_base + file_name + "_cover.jpg"
-	pic_small = pic_base + file_name + "_small.jpg"
-	post = "output/_posts/" + file_name + ".md"
+    file_name = start_date + "-" + event["name"].replace(" ", "-").replace("/", "").replace('"', "").replace("'", "")
+    pic_base = "output/public/images/thumbnails/"
+    pic = pic_base + file_name + ".jpg"
+    pic_big = pic_base + file_name + "_big.jpg"
+    pic_cover = pic_base + file_name + "_cover.jpg"
+    pic_small = pic_base + file_name + "_small.jpg"
+    post = "output/_posts/" + file_name + ".md"
 
-	template = template.replace("[TITLE]", event["name"].replace("'", "").replace('"', ""))
-	template = template.replace("[DESCRIPTION]", event["location"] + " " + event["description"][:20] + "...")	
-	template = template.replace("[TAGLINE]", start_date)
-	template = template.replace("[THUMBNAIL]", file_name + ".jpg")
-	template = template.replace("[BODY]", "Start time: " + event["start_time"] + "  \n" + event["description"])
-	# One of the posts has **** before and after text which messes up the template engine
-	template = template.replace("****", "###")
-	# One of the posts has some unicode that wasn't saving correctly
-	template = template.encode("ascii", "ignore")
+    template = template.replace("[TITLE]", event["name"].replace("'", "").replace('"', ""))
 
-	file = open(post, "w")
-	file.write(template)
-	file.close()
+    try:
+        template = template.replace("[DESCRIPTION]", event["location"] + " " + event["description"][:20] + "...")   
+    except KeyError:
+        pass
+    template = template.replace("[TAGLINE]", start_date)
+    template = template.replace("[THUMBNAIL]", file_name + ".jpg")
+    try:
+        template = template.replace("[BODY]", "Start time: " + event["start_time"] + "  \n" + event["description"])
+    except KeyError:
+        template = template.replace("[BODY]", "Start time: " + event["start_time"] + "  ")
+    # One of the posts has **** before and after text which messes up the template engine
+    template = template.replace("****", "###")
+    # One of the posts has some unicode that wasn't saving correctly
+    template = template.encode("ascii", "ignore")
 
-	retrievePicture(event["pic"], pic)
-	retrievePicture(event["pic_big"], pic_big)
-	if event["pic_cover"] is not None:
-		retrievePicture(event["pic_cover"]["source"], pic_cover)
-	retrievePicture(event["pic_small"], pic_small)
+    file = open(post, "w")
+    file.write(template)
+    file.close()
 
- 
-#"name": "Network Sys Tech Student CCNA study group", 
-#      "description": "Canyon room 123", 
-#      "start_time": "2013-05-29T18:00:00-0600", 
-#      "end_time": "2013-05-29T21:00:00-0600", 
-#      "location": "College of Southern Idaho", 
-#      "host": "SocialGeeks", 
+    retrievePicture(event["picture"]["data"]["url"], pic)
+    retrievePicture(event["picture"]["data"]["url"], pic_big)
+    retrievePicture(event["picture"]["data"]["url"], pic_cover)
+    retrievePicture(event["picture"]["data"]["url"], pic_small)
+
+#    {
+#      "name": "Turkey Lan planning night",
+#      "description": "We will be planning and ironing out all the details for the turkey lan.  If you plan on coming to the LAN, pl
+#an on coming to help get the stuff ready!",
+#      "start_time": "2011-11-02T19:00:00",
+#      "end_time": "2011-11-02T21:00:00",
+#      "location": "CSI Sub",
 #      "venue": {
-#        "latitude": 42.580160676872, 
-#        "longitude": -114.47447860752, 
-#        "city": "Twin Falls", 
-#        "state": "ID", 
-#        "country": "United States", 
-#        "id": 161280523619, 
-#        "street": "315 Falls Avenue", 
-#        "zip": "83303"
-#      }, 
-#      "pic": "https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-ash4/c225.0.264.264/s296x100/295369_10151592138587432_1816131650_n.jpg", 
-#      "pic_big": "https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-ash4/c225.0.264.264/s552x414/295369_10151592138587432_1816131650_n.jpg", 
-#      "pic_cover": {
-#        "cover_id": "10151592138587432", 
-#        "source": "https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-ash4/295369_10151592138587432_1816131650_n.jpg", 
-#        "offset_y": 0, 
-#        "offset_x": 0
-#      }, 
-#      "pic_small": "https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-ash4/c225.0.264.264/s160
+#        "street": "",
+#        "city": "",
+#        "state": "",
+#        "country": ""
+#      },
+#      "id": "248187198566972",
+#      "picture": {
+#        "data": {
+#          "is_silhouette": true,
+#          "url": "https://fbcdn-profile-a.akamaihd.net/static-ak/rsrc.php/v2/yE/r/tKlGLd_GmXe.png"
+#        }
+#      }
+#    },
 
